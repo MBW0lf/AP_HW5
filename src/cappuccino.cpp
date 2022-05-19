fe1b3085bdbd23ad6d4385f01ec9981586400c07
Mohammad <reyneh.100.mb@gmail.com>
2022-05-19 11:44:05 +0000

    Espresso_based and Cappuccino Created

--- src/cappuccino.cpp
+++ src/cappuccino.cpp
@@ -1,1 +1,61 @@
 #include "cappuccino.h"
+#include "sub_ingredients.h"
+
+//////////////////////////////////////////////////////////
+Cappuccino::Cappuccino()
+{
+    side_items.clear();
+    name = "Cappuccino";
+    ingredients.push_back(new Espresso{2});
+    ingredients.push_back(new Milk{2});
+    ingredients.push_back(new MilkFoam{1});
+}
+//////////////////////////////////////////////////////////
+Cappuccino::Cappuccino(const Cappuccino& cap)
+{
+    side_items = cap.side_items;
+    name = cap.name;
+    ingredients = cap.ingredients;
+}
+//////////////////////////////////////////////////////////
+void Cappuccino::operator=(const Cappuccino& cap)
+{
+    side_items = cap.side_items;
+    name = cap.name;
+    ingredients = cap.ingredients;
+}
+//////////////////////////////////////////////////////////
+double Cappuccino::price()
+{
+    double pr{0};
+    for(const auto& i : ingredients)
+        pr += i->price();
+    for(const auto& i : side_items)
+        pr += i->price();
+    return pr;
+}
+//////////////////////////////////////////////////////////
+std::string Cappuccino::get_name()
+{   
+    return name;
+}
+//////////////////////////////////////////////////////////
+void Cappuccino::add_side_item(Ingredient* side)
+{   
+    side_items.push_back(side);
+}
+//////////////////////////////////////////////////////////
+std::vector<Ingredient*>& Cappuccino::get_side_items()
+{
+    return side_items;
+}
+//////////////////////////////////////////////////////////
+Cappuccino::~Cappuccino()
+{
+    for(auto& i : side_items)
+    {
+        i = nullptr;
+        delete i;
+    }
+    side_items.clear();
+}
--- src/espresso_based.cpp
+++ src/espresso_based.cpp
@@ -1,1 +1,35 @@
 #include "espresso_based.h"
+
+//////////////////////////////////////////////////////////
+EspressoBased::EspressoBased()
+{
+    ingredients.clear();
+    name = "";
+}
+//////////////////////////////////////////////////////////
+EspressoBased::EspressoBased(const EspressoBased& esp)
+{
+    ingredients = esp.ingredients;
+    name = esp.name;
+}
+//////////////////////////////////////////////////////////
+void EspressoBased::operator=(const EspressoBased& esp)
+{
+    ingredients = esp.ingredients;
+    name = esp.name;
+}
+//////////////////////////////////////////////////////////
+std::vector<Ingredient*>& EspressoBased::get_ingredients()
+{
+    return ingredients;
+}
+//////////////////////////////////////////////////////////
+EspressoBased::~EspressoBased()
+{
+    for(auto& i : ingredients)
+    {
+        i = nullptr;
+        delete i;
+    }
+    ingredients.clear();
+}
